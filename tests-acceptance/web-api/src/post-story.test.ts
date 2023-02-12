429b916cd1e6db2d0b8427abca800bfb19cc9b44
Ilya Shestakov <siavol@gmail.com>
2023-02-12 18:59:54 +0200

    Use jest for api acceptance tests

---
+++ tests-acceptance/web-api/src/post-story.test.ts
@@ -0,0 +1,69 @@
+import {describe, expect, test, beforeAll} from '@jest/globals';
+
+describe('POST /story', () => {
+    let baseUrl: string;
+    
+    beforeAll(() => {
+        expect(process.env.APP_HOST).toBeDefined();
+        expect(process.env.APP_PORT).toBeDefined();
+
+        baseUrl = `http://${process.env.APP_HOST}:${process.env.APP_PORT}`;
+    });
+    
+    describe('When requesting with valid JSON body', () => {
+        let res: Response;
+        
+        beforeAll(async () => {
+            res = await fetch(`${baseUrl}/story`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ title: 'Test story' })
+            });
+        });
+        
+        test('should respond with HTTP 200', () => {
+            expect(res.status).toBe(200);
+        });
+        
+        test('should respond with body having story id', async () => {
+            expect(await res.json()).toEqual({
+                id: expect.any(String)
+            });
+        });
+    });
+    
+    describe('When requesting with incorrect "content-type" header value', () => {
+        let res: Response;
+
+        beforeAll(async () => {
+            res = await fetch(`${baseUrl}/story`, {
+                method: 'POST',
+                body: JSON.stringify({ title: 'Test story' })
+            });
+        });
+
+        test('should respond with HTTP 415 Unsupported Media Type', () => {
+            expect(res.status).toBe(415);
+        });
+    });
+
+    describe('When requesting with incorrect JSON in the body', () => {
+        let res: Response;
+
+        beforeAll(async () => {
+            res = await fetch(`${baseUrl}/story`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ foo: 'wtf' })
+            });
+        });
+
+        test('should respond with HTTP 400 Bad Request', () => {
+            expect(res.status).toBe(400);
+        });
+    });
+});
