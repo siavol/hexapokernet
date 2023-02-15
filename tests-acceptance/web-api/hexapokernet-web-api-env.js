1d9fd00e1a04ce86b220f559b53f3373a58769ec
Ilya Shestakov <siavol@gmail.com>
2023-02-15 10:56:34 +0200

    Add fetch logging to the acceptance tests

--- tests-acceptance/web-api/hexapokernet-web-api-env.js
+++ tests-acceptance/web-api/hexapokernet-web-api-env.js
@@ -1,19 +1,34 @@
 const assert = require('node:assert');
 const NodeEnvironment = require('jest-environment-node').TestEnvironment;
 
+const waitForMs = delay => new Promise(resolve => setTimeout(resolve, delay));
+
 class HexapokernetWebApiEnvironment extends NodeEnvironment {
     async setup() {
         await super.setup();
 
-        this.setGlobalBaseUrl();
+        this.global.baseUrl = this.getGlobalBaseUrl();
+        this.global.fetch = this.getFetchWithLogging();
+
         await this.waitAppHealthy();
     }
     
-    setGlobalBaseUrl() {
+    getGlobalBaseUrl() {
         assert(process.env.APP_HOST, 'Hexapokernet web API host must be set in APP_HOST environment variable');
         assert(process.env.APP_PORT, 'Hexapokernet web API port must be set in APP_PORT environment variable');
-        const baseUrl = `http://${process.env.APP_HOST}:${process.env.APP_PORT}`;
-        this.global.baseUrl = baseUrl;
+        return `http://${process.env.APP_HOST}:${process.env.APP_PORT}`;
+    }
+
+    getFetchWithLogging() {
+        return function(resource, options) {
+            const method = (options && options.method) || 'GET';
+            console.log(`[FETCH-REQ] ${method} ${resource}`);
+            return global.fetch(resource, options)
+                .then(res => {
+                    console.log(`[FETCH-RES] HTTP ${res.status}`);
+                    return res;
+                });
+        }
     }
     
     async waitAppHealthy() {
@@ -21,6 +36,9 @@ class HexapokernetWebApiEnvironment extends NodeEnvironment {
             const res = await fetch(`${this.global.baseUrl}/health`);
             if (res.ok) {
                 return;
+            } else {
+                console.log(`GET /health returns HTTP ${res.status}. Waiting app to healthy.`);
+                await waitForMs(500);
             }
         }
     }
